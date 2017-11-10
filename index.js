13f9bf1ffcff25363910db426f27e678e146a74a
Thomas Jascur <tjascur@yahoo.com>
2017-11-10 01:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Crazy Horse';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = '';
